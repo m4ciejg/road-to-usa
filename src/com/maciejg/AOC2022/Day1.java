85ec8ea10de400c564e8de9180c686a52d81cbab
m4ciejg <maciekgrabowski1996@gmail.com>
2022-12-05 01:07:57 +0100

    utils

--- src/com/maciejg/AOC2022/Day1.java
+++ src/com/maciejg/AOC2022/Day1.java
@@ -3,6 +3,5 @@ package com.maciejg.AOC2022;
 public class Day1 {
 
     public static void main(String[] args) {
-
     }
 }
--- src/com/maciejg/AOC2022/Utils.java
+++ src/com/maciejg/AOC2022/Utils.java
@@ -1,4 +1,40 @@
 package com.maciejg.AOC2022;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Utils {
+
+    private final static String finalPathHome = "C:/Users/Maciek/IdeaProjects/AdventOfCode/src/com/maciejg/";
+    private final static String finalPathWork = "C:/Users/maciej.grabowski/Documents/Projekty/PRYWATNE/src/com/maciejg/";
+
+    public static List<Integer> loadFileToList(String txt){
+        var temp = new ArrayList<Integer>();
+        try{
+            Scanner sc = new Scanner(new File(finalPathWork + txt));
+            while(sc.hasNext()) {
+                temp.add(Integer.parseInt(sc.nextLine()));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return temp;
+    }
+
+    public static List<String> loadFileToListString(String txt){
+        var temp = new ArrayList<String>();
+        try{
+            Scanner sc = new Scanner(new File(finalPathWork + txt));
+            while(sc.hasNext()) {
+                temp.add(sc.nextLine());
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return temp;
+    }
 }
